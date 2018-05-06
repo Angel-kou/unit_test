c22c8c707e6dd918b400a96cdcd6e0f63bf87af3
Angel-kou <1844646307@qq.com>
2018-05-06 21:44:23 +0800

    verify if generate an valid answer by kmj 2018/5/6

--- src/test/java/tw/core/generator/AnswerGeneratorTest.java
+++ src/test/java/tw/core/generator/AnswerGeneratorTest.java
@@ -1,9 +1,36 @@
 package tw.core.generator;
 
+import org.junit.Before;
+import org.junit.Test;
+import tw.core.exception.OutOfRangeAnswerException;
+
+import static junit.framework.TestCase.assertNotNull;
+
 /**
  * 在AnswerGeneratorTest文件中完成AnswerGenerator中对应的单元测试
  */
 public class AnswerGeneratorTest {
+
+
+    private RandomIntGenerator randomIntGenerator;
+    private AnswerGenerator answerGenerator;
+
+    @Before
+    public void setUp() {
+        randomIntGenerator =new RandomIntGenerator();
+        answerGenerator = new AnswerGenerator(randomIntGenerator);
+    }
+
+    @Test
+    public void should_not_throw_exception_when_call_generator() throws OutOfRangeAnswerException {
+        answerGenerator.generate();
+    }
+
+    @Test
+    public void should_return_an_answer_when_call_generator() throws OutOfRangeAnswerException {
+        assertNotNull("answer is not null",answerGenerator.generate().toString());
+    }
+
 
 }
 
